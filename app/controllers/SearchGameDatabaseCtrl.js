e538f037c6146de140c22b5e7d86f307885a5ff4
David Cramb <david.cramb@gmail.com>
2016-07-07 22:50:01 -0500

    search now filters results if a game currently exists in your backlog

--- app/controllers/SearchGameDatabaseCtrl.js
+++ app/controllers/SearchGameDatabaseCtrl.js
@@ -1,9 +1,46 @@
 "use strict";
-app.controller('SearchGameDatabaseCtrl', function($scope, $http, gameStorage,AuthFactory){
+app.controller('SearchGameDatabaseCtrl', function($document, $scope, $http, gameStorage,AuthFactory){
 
   $scope.searchForGameName = "";
   $scope.searchForGameNameResults = [];
+  $scope.gamesFilteredByUID = [];
   
+  $scope.searchGameDatabase = () => {
+    console.log('searching for Game:', $scope.searchForGameName)
+    gameStorage.searchGiantBombDatabase($scope.searchForGameName)
+      .then(function(searchResults){
+        $scope.searchForGameNameResults = searchResults.results;
+        $scope.filterResultsInBacklog($scope.searchForGameNameResults)
+      })
+  }; //searchGameDatabase
+
+  //takes results that already exist in backlog, styles the card and disables the button
+  $scope.filterResultsInBacklog = (title) => {
+    let user = AuthFactory.getUser();
+    gameStorage.populateBacklogPage()
+      .then(function(gamesList){
+        Object.keys(gamesList).forEach(function(key){
+        if (gamesList[key].gameObject.uid === user.uid){
+          Object.keys($scope.searchForGameNameResults).forEach(function(key2){
+            if ($scope.searchForGameNameResults[key2].id === gamesList[key].gameObject.id){
+              $scope.disableCardInBacklog($scope.searchForGameNameResults[key2].id);
+            };
+          });
+        };
+      });
+    });
+  };
+
+  $scope.disableCardInBacklog = (gameID) => {
+    let gameToStyle = $document[0].getElementById(`${gameID}`)
+    console.log(gameToStyle)
+    gameToStyle.setAttribute("id", "game-search-card-selected")
+    gameToStyle.remove() // hell with it just remove the damn thing
+
+  };
+
+
+
   $scope.gameCardSelected = (title, button, card) => {
     var buttonToDisable = button[0];
     var cardToStyle = card[1];
@@ -12,14 +49,6 @@ app.controller('SearchGameDatabaseCtrl', function($scope, $http, gameStorage,Aut
     buttonToDisable.setAttribute("class", "disabled");
   };
 
-  $scope.searchGameDatabase = () => {
-    console.log('searching for Game:', $scope.searchForGameName)
-    gameStorage.searchGiantBombDatabase($scope.searchForGameName)
-      .then(function(searchResults){
-        $scope.searchForGameNameResults = searchResults.results;
-        console.log($scope.searchForGameNameResults);
-      });
-  }; //searchGameDatabase
 
   $scope.addGametoUserBacklog = (uniqueGameID, title, $event) => {
     console.log('adding title to backlog ->', title);
